af933e0cdc56dcbd7ca64f6e6fad5b691c05e841
akurfurst <19035144+akurfurst@users.noreply.github.com>
2026-04-17 10:14:48 -0700

    Implemented predictNextWord() and passed all tests

--- src/UnigramWordPredictor.java
+++ src/UnigramWordPredictor.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A class for predicting the next word in a sequence using a unigram model.
@@ -130,7 +131,14 @@ public class UnigramWordPredictor implements WordPredictor {
   public String predictNextWord(List<String> context) {
     // TODO: Return a predicted word given the words preceding it
     // Hint: only the last word in context should be looked at
-    return null;
+    String result = "";
+    Random rng = new Random();
+    String word = context.get(context.size() -1);
+    List<String> neighbors = neighborMap.get(word);
+    result = neighbors.get(rng.nextInt(0, neighbors.size()));
+
+
+    return result;
   }
   
   /**
